4b17db4397ddb6e604024da4e5a18eaad8b3b84b
matveisever100-oss <matveisever100@gmail.com>
2025-10-05 15:15:52 +0300

    Add files via upload

--- Flappy Cat/service-worker.js
+++ Flappy Cat/service-worker.js
@@ -1,4 +1,6 @@
-const CACHE = "flappycat-v1";
-const FILES = ["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
+const CACHE_NAME = "flappycat_mobile_v2";
+const FILES = ["./","./index.html","./manifest.json","./service-worker.js","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE_NAME ? caches.delete(k):null)))); self.clients.claim(); });
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
